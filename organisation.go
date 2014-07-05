6e0644faa09bc03ac671ca17221b6e3e7c17868d
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-05 23:12:10 +0100

    Debugging

--- organisation.go
+++ organisation.go
@@ -31,7 +31,7 @@ func (o *Organisations) GetAll (session *VCloudSession, format string, max_page_
     for i := 1; i <= max_pages; i++ {
         uri := fmt.Sprintf("/api/query?type=organization&format=%v&pageSize=%v&page=%v", format, max_page_size, i)
 
-        fmt.Printf("I = %v | Url = %s", i, url)
+        fmt.Printf("I = %v | Uri = %s", i, uri)
 
         r := session.Get(uri)
         defer r.Body.Close()

87fd2d0148784e2939a961389a7bae887423b985
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-05 21:34:48 +0100

    Fixing up spaces.

--- config.go
+++ config.go
@@ -2,24 +2,24 @@
 package vcloudoslicenses 
 
 type vCloud struct {
-	Hostname 	string 
-	Port 		string 
-	Username 	string 
-	Password 	string 
-	Context 	string
+    Hostname    string 
+    Port        string 
+    Username    string 
+    Password    string 
+    Context     string
 }
 
 type ElasticSearch struct {
-	Hostname 	string 
-	Port 		string 
-	Index		string 
-	DocType	 	string
+    Hostname    string 
+    Port        string 
+    Index       string 
+    DocType     string
 }
 
 type Configuration struct {
-	vCloud 			vCloud
-	ElasticSearch 	ElasticSearch
-	Session 		*vCloudToken 
+    vCloud          vCloud
+    ElasticSearch   ElasticSearch
+    Session         *vCloudToken 
 
-	Debugging 		bool
+    Debugging       bool
 }
--- vm.go
+++ vm.go
@@ -7,30 +7,30 @@ import (
 )
 
 type VMRecordOSType struct {
-	Name string `xml:"osType,attr"`
+    Name string `xml:"osType,attr"`
 
-	Href string `xml:"vcloud:href,attr"`
-	Type string `xml:"vcloud:type,attr"`
+    Href string `xml:"vcloud:href,attr"`
+    Type string `xml:"vcloud:type,attr"`
 }
 
 type VMRecord struct {
-	OSType VMRecordOSType `xml:"OperatingSystemSection"`
+    OSType VMRecordOSType `xml:"OperatingSystemSection"`
 }
 
 type Child struct {
-	Server []*VMRecord `xml:"Vm"`
+    Server []*VMRecord `xml:"Vm"`
 }
 
 type VMs struct {
-	Records Child `xml:"Children"`
+    Records Child `xml:"Children"`
 }
 
 func (v *VMs) GetAll (session *vCloudSession, vapp *VAppLinkRecord) {
-	r := session.Get(vapp.Href)
-	defer r.Close()
+    r := session.Get(vapp.Href)
+    defer r.Close()
 
-	_ = xml.NewDecoder(r).Decode(v)
-	for _, v1 := range v.Records.Server {
+    _ = xml.NewDecoder(r).Decode(v)
+    for _, v1 := range v.Records.Server {
         u, _ := url.Parse(v1.OSType.Href)
         v1.OSType.Href = u.Path 
     }

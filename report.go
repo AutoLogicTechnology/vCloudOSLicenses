bc86f1e4ba7ad3e3af1df73b171ba152b863ebd9
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-06 13:55:57 +0100

    Moving things around. Trying out a new structure/design.

--- report.go
+++ report.go
@@ -30,66 +30,66 @@ type WorkerJob struct {
     Organisation    *OrganisationReference
 }
 
-func (v *VCloudSession) ReportWorker (job *WorkerJob) {
-    log.Print("Inside the worker...")
+// func (v *VCloudSession) ReportWorker (job *WorkerJob) {
+//     log.Print("Inside the worker...")
 
-    vdcs := &VDCs{}
-    vdcs.GetAll(v, job.Organisation)
+//     vdcs := &VDCs{}
+//     vdcs.GetAll(v, job.Organisation)
 
-    for _, vdc := range vdcs.Records {
-        log.Print("Going over VDCs...")
+//     for _, vdc := range vdcs.Records {
+//         log.Print("Going over VDCs...")
 
-        if vdc.Type == "application/vnd.vmware.vcloud.vdc+xml" {
-            vapps := &vApps{}
-            vapps.GetAll(v, vdc)   
+//         if vdc.Type == "application/vnd.vmware.vcloud.vdc+xml" {
+//             vapps := &vApps{}
+//             vapps.GetAll(v, vdc)   
 
-            for _, vapp := range vapps.Records.Entities {
-                log.Print("Going over vApps...")
+//             for _, vapp := range vapps.Records.Entities {
+//                 log.Print("Going over vApps...")
 
-                if vapp.Type == "application/vnd.vmware.vcloud.vApp+xml" {
-                    vms := &VMs{}
-                    vms.GetAll(v, vapp)
+//                 if vapp.Type == "application/vnd.vmware.vcloud.vApp+xml" {
+//                     vms := &VMs{}
+//                     vms.GetAll(v, vapp)
 
-                    now := time.Now()
+//                     now := time.Now()
 
-                    report := &ReportDocument{
-                        Timestamp:      now.String(),
-                        Year:           strconv.Itoa(now.Year()),
-                        Month:          now.Month().String(),
-                        Day:            strconv.Itoa(now.Day()),
-                        Organisation:   job.Organisation.Name,
-                        VDC:            vdc.Name,
-                        VApp:           vapp.Name,
-                        MSWindows:      0,
-                        RHEL:           0,
-                        CentOS:         0,
-                        Ubuntu:         0,
-                    }
+//                     report := &ReportDocument{
+//                         Timestamp:      now.String(),
+//                         Year:           strconv.Itoa(now.Year()),
+//                         Month:          now.Month().String(),
+//                         Day:            strconv.Itoa(now.Day()),
+//                         Organisation:   job.Organisation.Name,
+//                         VDC:            vdc.Name,
+//                         VApp:           vapp.Name,
+//                         MSWindows:      0,
+//                         RHEL:           0,
+//                         CentOS:         0,
+//                         Ubuntu:         0,
+//                     }
 
-                    for _, vm := range vms.Records.Server {
-                        log.Print("And VMs...")
+//                     for _, vm := range vms.Records.Server {
+//                         log.Print("And VMs...")
 
-                        if strings.Contains(vm.OSType.Name, "windows") {
-                            report.MSWindows++
-                        } else if strings.Contains(vm.OSType.Name, "rhel") {
-                            report.RHEL++
-                        } else if strings.Contains(vm.OSType.Name, "centos") {
-                            report.CentOS++
-                        } else if strings.Contains(vm.OSType.Name, "ubuntu") {
-                            report.Ubuntu++
-                        }
-                    }
+//                         if strings.Contains(vm.OSType.Name, "windows") {
+//                             report.MSWindows++
+//                         } else if strings.Contains(vm.OSType.Name, "rhel") {
+//                             report.RHEL++
+//                         } else if strings.Contains(vm.OSType.Name, "centos") {
+//                             report.CentOS++
+//                         } else if strings.Contains(vm.OSType.Name, "ubuntu") {
+//                             report.Ubuntu++
+//                         }
+//                     }
 
-                    log.Printf("Report from worker: %+v", report)
-                    job.ResultsChannel <- report
-                }
-            }
-        }
-    }
+//                     log.Printf("Report from worker: %+v", report)
+//                     job.ResultsChannel <- report
+//                 }
+//             }
+//         }
+//     }
 
-    log.Print("Worker done...")
-    job.Waiter.Done() 
-}
+//     log.Print("Worker done...")
+//     job.Waiter.Done() 
+// }
 
 func (v *VCloudSession) LicenseReport (max_organisations, max_pages int) (report []*ReportDocument) {
     var reports []*ReportDocument
@@ -102,10 +102,10 @@ func (v *VCloudSession) LicenseReport (max_organisations, max_pages int) (report
         max_pages = 1
     }
 
-    waiter  := &sync.WaitGroup{}
-    results := make(chan *ReportDocument)
+    // waiter  := &sync.WaitGroup{}
+    // results := make(chan *ReportDocument)
 
-    waiter.Add(max_organisations)
+    // waiter.Add(max_organisations)
 
     orgs, _ := FindOrganisations(v, 10, 1)
     // orgs.GetAll(v, "references", max_organisations, max_pages)

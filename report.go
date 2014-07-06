140c5aac2fb48a848f44701c6e05f52d5ce88a5f
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-06 22:45:39 +0100

    Bugs in new idea.

--- report.go
+++ report.go
@@ -102,14 +102,68 @@ func (v *VCloudSession) ReportWorker (job *WorkerJob) {
     job.Waiter.Done() 
 }
 
-func (v *VCloudSession) VAppReportWorker (jobs <- chan *VAppWorkerJob) {
+// func (v *VCloudSession) VAppReportWorker (jobs <- chan *VAppWorkerJob) {
 
-    for job := range jobs {
-        log.Printf("Job: %+v", job)
+//     for job := range jobs {
+//         log.Printf("Job: %+v", job)
 
+//         vdc := &VDCVApp{}
+
+//         r := v.Get(job.VApp.Href)
+//         defer r.Body.Close()
+
+//         if r.StatusCode != 200 {
+//             continue 
+//         }
+
+//         _ = xml.NewDecoder(r.Body).Decode(vdc)
+
+//         now := time.Now()
+//         report := &ReportDocument{
+//             Timestamp:      now.String(),
+//             Year:           strconv.Itoa(now.Year()),
+//             Month:          now.Month().String(),
+//             Day:            strconv.Itoa(now.Day()),
+//             Organisation:   job.VApp.OwnerName,
+//             VDC:            job.VApp.VDCName,
+//             VApp:           job.VApp.Name,
+//             MSWindows:      0,
+//             RHEL:           0,
+//             CentOS:         0,
+//             Ubuntu:         0,
+//             Unknown:        0,
+//         }
+ 
+//         for _, vm := range vdc.VMs.VM {
+//             v.Counters.VMs++
+
+//             if strings.Contains(vm.OperatingSystemSection.OSType, "windows") {
+//                 report.MSWindows++
+//             } else if strings.Contains(vm.OperatingSystemSection.OSType, "rhel") {
+//                 report.RHEL++
+//             } else if strings.Contains(vm.OperatingSystemSection.OSType, "centos") {
+//                 report.CentOS++
+//             } else if strings.Contains(vm.OperatingSystemSection.OSType, "ubuntu") {
+//                 report.Ubuntu++
+//             } else {
+//                 report.Unknown++
+//             }
+//         }
+
+//         log.Printf("Report: %+v", report)
+
+//         job.ResultsChannel <- report
+//     }
+
+//     log.Print("Worker finished...")
+// }
+
+func (v *VCloudSession) VAppReport (max_vapps, max_pages int) (reports []*ReportDocument) {
+    vapps, _ := v.FindVApps(max_vapps, max_pages)
+    for _, vapp := range vapps {
         vdc := &VDCVApp{}
 
-        r := v.Get(job.VApp.Href)
+        r := v.Get(vapp.Href)
         defer r.Body.Close()
 
         if r.StatusCode != 200 {
@@ -124,9 +178,9 @@ func (v *VCloudSession) VAppReportWorker (jobs <- chan *VAppWorkerJob) {
             Year:           strconv.Itoa(now.Year()),
             Month:          now.Month().String(),
             Day:            strconv.Itoa(now.Day()),
-            Organisation:   job.VApp.OwnerName,
-            VDC:            job.VApp.VDCName,
-            VApp:           job.VApp.Name,
+            Organisation:   vapp.OwnerName,
+            VDC:            vapp.VDCName,
+            VApp:           vapp.Name,
             MSWindows:      0,
             RHEL:           0,
             CentOS:         0,
@@ -150,45 +204,7 @@ func (v *VCloudSession) VAppReportWorker (jobs <- chan *VAppWorkerJob) {
             }
         }
 
-        log.Printf("Report: %+v", report)
-
-        job.ResultsChannel <- report
-    }
-
-    log.Print("Worker finished...")
-}
-
-func (v *VCloudSession) VAppReport (max_vapps, max_pages int) (reports []*ReportDocument) {
-    // waiter  := &sync.WaitGroup{}
-    results := make(chan *ReportDocument)
-    jobs    := make(chan *VAppWorkerJob)
-
-    for i := 1; i <= 10; i++ {
-        log.Print("Creating worker...")
-
-        go v.VAppReportWorker(jobs)
-    }
-
-    vapps, _ := v.FindVApps(max_vapps, max_pages)
-    for _, vapp := range vapps {
-        // waiter.Add(1)
-
-        job := &VAppWorkerJob{
-            // Waiter:         waiter,
-            ResultsChannel: results,
-            VApp:           vapp, 
-        }
-
-        jobs <- job 
-    }
-    close(jobs)
-
-    // go func() {
-    //     waiter.Wait()
-    //     close(results)
-    // }()
-
-    for report := range results {
+        log.Printf("Report: %+v", report) 
         reports = append(reports, report)
     }
   

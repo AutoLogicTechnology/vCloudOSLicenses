0b54a3d021b90f1ae6f5125293b44c7d077f8b5a
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-06 18:36:12 +0100

    Debugging... hold tight. Typos.

--- vapp.go
+++ vapp.go
@@ -52,9 +52,10 @@ func (a *VApp) Get (session *VCloudSession, vdc *VdcResourceEntity) {
     r := session.Get(vdc.Href)
     defer r.Body.Close()
 
+    _ = xml.NewDecoder(r.Body).Decode(a)
+
     log.Printf("vApp Children Selfie: %+v", a.Children.Child)
 
-    _ = xml.NewDecoder(r.Body).Decode(a)
     for k1, v1 := range a.Children.Child {
         u, _ := url.Parse(v1.Href)
         a.Children.Child[k1].Href = u.Path 

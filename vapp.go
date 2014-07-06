6fbad513b3d63551cc7bc926e4c677a99dd6035b
Michael Crilly <mcrilly@skyscapecloud.com>
2014-07-06 18:48:13 +0100

    Debugging... hold tight. Typos.

--- vapp.go
+++ vapp.go
@@ -3,7 +3,7 @@ package vcloudoslicenses
 
 import (
     "log"
-    "io/ioutil"
+    // "io/ioutil"
 
     "encoding/xml"
     "net/url"
@@ -46,27 +46,27 @@ type VApp struct {
     Type        string `xml:"deployed,attr"`
     Href        string `xml:"deployed,attr"`
 
-    Children    VAppChildren `xml:"Children"`
+    // Children    VAppChildren `xml:"Children"`
 }
 
 func (a *VApp) Get (session *VCloudSession, vdc *VdcResourceEntity) {
     r := session.Get(vdc.Href)
     defer r.Body.Close()
 
-    raw, _ := ioutil.ReadAll(r.Body)
-    log.Printf("RAW VAPP BODY: %v", string(raw))
+    // raw, _ := ioutil.ReadAll(r.Body)
+    // log.Printf("RAW VAPP BODY: %v", string(raw))
 
     _ = xml.NewDecoder(r.Body).Decode(a)
 
     log.Printf("vApp Children Selfie: %+v", a)
 
-    for k1, v1 := range a.Children.Child {
-        u, _ := url.Parse(v1.Href)
-        a.Children.Child[k1].Href = u.Path 
+    // for k1, v1 := range a.Children.Child {
+    //     u, _ := url.Parse(v1.Href)
+    //     a.Children.Child[k1].Href = u.Path 
 
-        u, _ = url.Parse(v1.OperatingSystemSection.Href)
-        a.Children.Child[k1].OperatingSystemSection.Href = u.Path
-    }
+    //     u, _ = url.Parse(v1.OperatingSystemSection.Href)
+    //     a.Children.Child[k1].OperatingSystemSection.Href = u.Path
+    // }
 
     // log.Printf("vApp Selfie: %+v", a)
 }
